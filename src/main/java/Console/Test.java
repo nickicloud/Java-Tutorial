f83a3946dc3b822e9ecd42cf3d07cb92a8808531
Nicki <nickicloudservice@gmail.com>
2025-12-24 13:28:11 +0100

    initial commit

---
+++ src/main/java/Console/Test.java
@@ -0,0 +1,31 @@
+/*
+ *
+ *  * Copyright (c) 2025-2025 Nicklas Voß (nickicloud.de)
+ *  * Alle Rechte vorbehalten / All rights reserved.
+ *  *
+ *  * DE:
+ *  * Dieses Werk ist urheberrechtlich geschützt. Jegliche Vervielfältigung,
+ *  * Verbreitung, Speicherung, Verarbeitung oder andere Nutzung ist ohne
+ *  * ausdrückliche schriftliche Zustimmung des Rechteinhabers untersagt.
+ *  *
+ *  * EN:
+ *  * This work is protected by copyright law. Any reproduction, distribution,
+ *  * storage, processing, or other use is prohibited without the express
+ *  * written permission of the rights holder.
+ *  *
+ *  * ------------------------------------------------------------
+ *  * ------------------------------------------------------------
+ *  *
+ *  * Datei: $file.name
+ *  * Erstellt: 01.12.25, 18:39
+ *
+ */
+
+package Console;
+
+public class Test {
+    public static void start() {
+        System.out.println("Hello World");
+
+    }
+}
---
+++ src/main/java/Library/Library.java
@@ -0,0 +1,34 @@
+/*
+ *
+ *  * Copyright (c) 2025-2025 Nicklas Voß (nickicloud.de)
+ *  * Alle Rechte vorbehalten / All rights reserved.
+ *  *
+ *  * DE:
+ *  * Dieses Werk ist urheberrechtlich geschützt. Jegliche Vervielfältigung,
+ *  * Verbreitung, Speicherung, Verarbeitung oder andere Nutzung ist ohne
+ *  * ausdrückliche schriftliche Zustimmung des Rechteinhabers untersagt.
+ *  *
+ *  * EN:
+ *  * This work is protected by copyright law. Any reproduction, distribution,
+ *  * storage, processing, or other use is prohibited without the express
+ *  * written permission of the rights holder.
+ *  *
+ *  * ------------------------------------------------------------
+ *  * ------------------------------------------------------------
+ *  *
+ *  * Datei: $file.name
+ *  * Erstellt: 30.11.25, 16:11
+ *
+ */
+package Library;
+
+import java.util.*;
+public class Library {
+    public static Scanner scanner = new Scanner(System.in);
+    public static String Read() {
+        return scanner.nextLine();
+
+
+
+    }
+}
